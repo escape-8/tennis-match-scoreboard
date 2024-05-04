7c4bedc3ba8bcd50086d580094ad4930f9b49d37
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-05-04 13:20:23 +0300

    feat: implement PlayersRepository

---
+++ src/Repository/PlayersRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Model\Player;
+use Doctrine\ORM\EntityManager;
+
+class PlayersRepository
+{
+    private EntityManager $ORM;
+
+    public function __construct(EntityManager $ORM)
+    {
+        $this->ORM = $ORM;
+    }
+
+    public function add(Player $player): void
+    {
+        $this->ORM->persist($player);
+    }
+
+    public function playerExists(string $name): bool
+    {
+        $player = $this->ORM->getRepository(Player::class)->findOneBy(['name' => $name]);
+
+        if ($player) {
+            return true;
+        }
+
+        return false;
+    }
+}
