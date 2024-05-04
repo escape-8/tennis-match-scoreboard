035050c3ac5a802e1f405ed5245288dd6a4b913a
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-05-04 13:18:41 +0300

    feat: implement OngoingMatchesService

---
+++ src/Service/OngoingMatchesService.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\DTO\NewMatchPlayersDTO;
+use App\Model\GameMatchScore;
+use App\Model\Player;
+use App\Repository\OngoingMatchesRepository;
+use App\Repository\PlayersRepository;
+use App\Util\Flusher;
+
+class OngoingMatchesService
+{
+    private PlayersRepository $playersRepository;
+    private Flusher $flusher;
+    private OngoingMatchesRepository $ongoingMatches;
+
+    public function __construct(PlayersRepository $playersRepository, Flusher $flusher, OngoingMatchesRepository $ongoingMatchesRepository)
+    {
+        $this->playersRepository = $playersRepository;
+        $this->flusher = $flusher;
+        $this->ongoingMatches = $ongoingMatchesRepository;
+    }
+
+    public function create(NewMatchPlayersDTO $matchPlayersDTO, string $uuid): void
+    {
+        foreach ($matchPlayersDTO as $playerName) {
+            if (!$this->playersRepository->playerExists($playerName)) {
+                $this->playersRepository->add(new Player($playerName));
+            }
+        }
+        $this->flusher->flush();
+        $this->ongoingMatches->add($uuid, new GameMatchScore($matchPlayersDTO->playerName1, $matchPlayersDTO->playerName2));
+    }
+
+    public function getMatch(string $uuid): GameMatchScore
+    {
+        return $this->ongoingMatches->get($uuid);
+    }
+}
