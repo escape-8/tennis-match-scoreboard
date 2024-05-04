4db5132bb110bb69c02cce9eed4b480a79026a3e
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-05-04 13:17:40 +0300

    feat: implement NewMatchController

--- config/routes.php
+++ config/routes.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewMatchController;
 use Slim\App;
 
 return static function (App $app): void
 {
-    $app->get('/', function (Request $request, Response $response, array $args) {
-        return $response;
-    });
     $app->get('/', [HomeController::class, 'index']);
+
+    $app->get('/new-match', [NewMatchController::class, 'index']);
+
+    $app->post('/new-match', [NewMatchController::class, 'create']);
 };
---
+++ src/Http/Controllers/NewMatchController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Service\OngoingMatchesService;
+use App\Util\Uuid;
+use App\Validator\NewMatchPlayersValidator;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\PhpRenderer;
+
+class NewMatchController
+{
+    private PhpRenderer $renderer;
+    private NewMatchPlayersValidator $matchPlayersValidator;
+    private OngoingMatchesService $ongoingMatchesService;
+
+
+    public function __construct(PhpRenderer $renderer, NewMatchPlayersValidator $matchPlayersValidator, OngoingMatchesService $ongoingMatchesService)
+    {
+        $this->renderer = $renderer;
+        $this->matchPlayersValidator = $matchPlayersValidator;
+        $this->ongoingMatchesService = $ongoingMatchesService;
+    }
+
+
+    public function index(Request $request, Response $response): Response
+    {
+        return $this->renderer->render($response, 'html/pages/new-match.phtml');
+    }
+
+    public function create(Request $request, Response $response): Response
+    {
+        $uuid = Uuid::uuid4();
+        $newMatchDTO = $this->matchPlayersValidator->validate($request->getParsedBody());
+        $this->ongoingMatchesService->create($newMatchDTO, $uuid);
+        return $response->withHeader('Location', '/match-score?' . http_build_query(['uuid' => $uuid]))->withStatus(302);
+    }
+}
