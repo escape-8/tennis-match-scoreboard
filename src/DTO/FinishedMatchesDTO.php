a891ce3c447f71103f8b2941c6ea11c56bc91a43
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-05-11 07:46:02 +0300

    feat: implement FinishedMatchesDTO

---
+++ src/DTO/FinishedMatchesDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+class FinishedMatchesDTO
+{
+    /**
+     * @var array<FinishMatchDTO> $finishedMatches
+     */
+    public readonly array $finishedMatches;
+    public readonly string $findPlayerName;
+    public readonly int $maxPages;
+    public readonly int $page;
+
+    public function __construct(array $finishedMatches, string $findPlayerName, int $maxPages, int $page = 1)
+    {
+        $this->finishedMatches = $finishedMatches;
+        $this->findPlayerName = $findPlayerName;
+        $this->maxPages = $maxPages;
+        $this->page = $page;
+    }
+}
